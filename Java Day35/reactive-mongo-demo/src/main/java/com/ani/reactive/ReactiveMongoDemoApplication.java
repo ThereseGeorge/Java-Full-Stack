c432e6c873021e1dd18f83228efcede0b3714c64
ThereseGeorge <theresevempeny1997@gmail.com>
2023-04-12 12:02:57 +0530

    Web-reactive

---
+++ Java Day35/reactive-mongo-demo/src/main/java/com/ani/reactive/ReactiveMongoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ani.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoDemoApplication.class, args);
+	}
+
+}
