f839049f62d3a009b2b766c3a8bfc709b6eac3b8
ThereseGeorge <theresevempeny1997@gmail.com>
2023-05-01 00:59:41 +0530

    orm-crud

---
+++ Java Day52/type-orm-crud/index.js
@@ -0,0 +1,39 @@
+const typeorm = require('typeorm')
+
+const dataSource = new typeorm.DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "therese123",
+    database: "nodejsdb",
+    synchronize: true,
+    entities: [ require("./Post"), require("./Category")],
+    logging: true,
+})
+
+dataSource.initialize().then(() => {
+    console.log(`✅ Successfully Created`)
+
+    var category1 = {
+        id: 1,
+        name: "Python",
+    }
+    var category2 = {
+        name: "Programming",
+    }
+
+    const categoryRepository = dataSource.getRepository('Category')
+    categoryRepository.save(category1).then(saved => {
+        console.log(`✅ Category Saved`)
+    }).catch(console.log)
+    categoryRepository.save(category2).then(saved => {
+        console.log(`✅ Category Saved`)
+    }).catch(console.log)
+
+    // categoryRepository.find().then(categories => console.log(categories)).catch(console.log)
+
+    // categoryRepository.delete(2).then(res => console.log(`✅ Deleted Successfully`)).catch(console.log)
+}).catch(error => {
+    console.log("❌ Error: ", error)
+})
